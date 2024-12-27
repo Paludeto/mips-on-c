4c2407392248b58953ae66cec5306364659dc420
Paludeto <gpalu54@gmail.com>
2024-12-27 11:34:46 -0300

    nuked instruction.h

--- include/instruction_list.h
+++ include/instruction_list.h
@@ -1,14 +1,30 @@
 #ifndef INSTRUCTION_LIST_H
 #define INSTRUCTION_LIST_H
 
-// This is for debugging purposes, we should probably use some sort of 
-#include "instruction.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "register.h"
+
+typedef enum InstructionType {
+    R,
+    I,
+    J,
+    UNKNOWN
+} InstructionType;
+
+// Dispatcher pattern, where we map the instruction's name to a function in a table via function pointers
+// Each instruction has its own executor function in the table
+typedef struct Instruction {
+    char *name;
+    char **operands;
+    InstructionType type;
+    int op_count;
+    void (*executor)(char **operands, Register *r_array);  
+} Instruction;
 
 typedef struct Node {
-    Instruction instruction;
+    struct Instruction *instruction;  // Use pointer to Instruction to break circular dependency
     struct Node *next;
 } Node;
 
@@ -17,9 +33,47 @@ typedef struct InstructionList {
     int size;
 } InstructionList;
 
+// Function declarations
 void init_instruction_list(InstructionList *list);
-void add_instruction(InstructionList *list, Instruction *inst);
-void print_instruction_list(InstructionList *list);
+void add_instruction(InstructionList *list, struct Instruction *inst);
+void print_instruction_list(const InstructionList *list);
 void free_instruction_list(InstructionList *list);
 
-#endif 
+// BLOAT
+void syscall(Instruction *inst, Register *arg, Register *dest);
+void binary(Instruction inst);
+
+// R-Type instructions
+void r_add(Register *rs, Register *rt, Register *rd);
+void r_sub(Register *rs, Register *rt, Register *rd);
+void r_mult(Register *rs, Register *rt, Register *rd);
+void r_and(Register *rs, Register *rt, Register *rd);
+void r_or(Register *rs, Register *rt, Register *rd);
+void r_sll(Register *rt, Register *rd, int shamt);
+void move(Register *rd, Register *rs);
+void slt(Register *rs, Register *rt, Register *rd);
+void slti(Register *rs, Register *rt, int imm);
+
+// I-Type instructions
+void i_addi(Register *rs, Register *rt, int imm);
+void i_lui(Register *rt, int16_t imm);
+
+// Instruction executors
+void execute_add(char **operands, Register *r_array);
+void execute_sub(char **operands, Register *r_array);
+void execute_mult(char **operands, Register *r_array);
+void execute_and(char **operands, Register *r_array);
+void execute_or(char **operands, Register *r_array);
+void execute_sll(char **operands, Register *r_array);
+void execute_lui(char **operands, Register *r_array);
+void execute_addi(char **operands, Register *r_array);
+void execute_move(char **operands, Register *r_array);
+void execute_slt(char **operands, Register *r_array);
+void execute_slti(char **operands, Register *r_array);
+
+// Instruction creation and validation
+Instruction *find_instruction(const char *name);
+Instruction *create_instruction(const char *name, char **operands, int op_count);
+void free_instruction(Instruction *inst);
+
+#endif
--- src/instruction_list.c
+++ src/instruction_list.c
@@ -1,17 +1,16 @@
+
 #include "instruction_list.h"
-
-// Needs to be rethinked and refactored for all possible cases
-
 
 // Initialize the instruction list
 void init_instruction_list(InstructionList *list) {
 
     list->head = NULL;
     list->size = 0;
+
 }
 
 // Add an instruction to the list
-void add_instruction(InstructionList *list, Instruction *inst) {
+void add_instruction(InstructionList *list, struct Instruction *inst) {
 
     Node *new_node = (Node *)malloc(sizeof(Node));
 
@@ -20,7 +19,7 @@ void add_instruction(InstructionList *list, Instruction *inst) {
         return;
     }
 
-    new_node->instruction = *inst;
+    new_node->instruction = inst;
     new_node->next = NULL;
 
     if (list->head == NULL) {
@@ -38,26 +37,21 @@ void add_instruction(InstructionList *list, Instruction *inst) {
 }
 
 // Print all instructions in the list
-void print_instruction_list(InstructionList *list) {
+void print_instruction_list(const InstructionList *list) {
 
     Node *current = list->head;
     int index = 1;
+
     while (current != NULL) {
 
-        if (current->instruction.type == R) {
-            printf("Instruction %d: %s", index++, current->instruction.name);
-            printf(" %s", current->instruction.operands[0]);
-            printf(" %s", current->instruction.operands[1]);
-            printf(" %s", current->instruction.operands[2]);
-            printf(", type %d\n", current->instruction.type);
-        } else if (current->instruction.type == I) {
-            printf("Instruction %d: %s", index++, current->instruction.name);
-            printf(" %s", current->instruction.operands[1]);
-            printf(" %s", current->instruction.operands[2]);
-            printf(", type %d\n", current->instruction.type);
+        struct Instruction *inst = current->instruction;
+        printf("Instruction %d: %s", index++, inst->name);
+        for (int i = 0; i < inst->op_count; i++) {
+            printf(" %s", inst->operands[i]);
         }
-        
+        printf(", type %d\n", inst->type);
         current = current->next;
+
     }
 
 }
@@ -66,13 +60,293 @@ void print_instruction_list(InstructionList *list) {
 void free_instruction_list(InstructionList *list) {
 
     Node *current = list->head;
-    Node *temp;
+
     while (current != NULL) {
-        temp = current;
+
+        Node *temp = current;
         current = current->next;
         free(temp);
+
     }
+
     list->head = NULL;
     list->size = 0;
+
+}
+
+// BLOAT STARTS HERE
+
+// Instruction table
+Instruction table[] = {
+    {"add", NULL, R, 3, execute_add},
+    {"sub", NULL, R, 3, execute_sub},
+    {"mult", NULL, R, 3, execute_mult},
+    {"and", NULL, R, 3, execute_and},
+    {"or", NULL, R, 3, execute_or},
+    {"sll", NULL, R, 3, execute_sll},
+    {"lui", NULL, I, 2, execute_lui},
+    {"addi", NULL, I, 3, execute_addi},
+    {"move", NULL, R, 2, execute_move},
+    {"slt", NULL, R, 3, execute_slt},
+    {NULL, NULL, UNKNOWN, 0, NULL} // Sentinel to mark the end
+};
+
+// TO-DO: implement behavior
+void syscall(Instruction *inst, Register *arg, Register *dest);
+void binary(Instruction inst);
+
+// Executors
+void execute_add(char **operands, Register *r_array) {
+    
+    printf("Executing ADD with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+
+    r_add(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_sub(char **operands, Register *r_array) {
+
+    printf("Executing SUB with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+    
+    r_sub(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_mult(char **operands, Register *r_array) {
+
+    printf("Executing MULT with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+
+    r_mult(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_and(char **operands, Register *r_array) {
+
+    printf("Executing AND with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+
+    r_and(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_or(char **operands, Register *r_array) {
+
+    printf("Executing OR with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+
+    r_or(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_sll(char **operands, Register *r_array) {
+
+    printf("Executing SLL with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rt = get_register_index(operands[1]);
+    int shamt = atoi(operands[2]);
+
+    r_sll(&r_array[rt], &r_array[rd], shamt);
+
+}
+
+void execute_lui(char **operands, Register *r_array) {
+
+    printf("Executing LUI with operands %s, %s\n", operands[0], operands[1]);
+
+    int rt = get_register_index(operands[0]);
+    int imm = atoi(operands[1]);
+
+    i_lui(&r_array[rt], imm);
+
+}
+
+void execute_addi(char **operands, Register *r_array) {
+
+    printf("Executing ADDI with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rs = get_register_index(operands[0]);
+    int rt = get_register_index(operands[1]);
+    int imm = atoi(operands[2]);
+
+    i_addi(&r_array[rs], &r_array[rt], imm);
+
+}
+
+void execute_move(char **operands, Register *r_array) {
+
+    printf("Executing MOVE with operands %s, %s\n", operands[0], operands[1]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+
+    move(&r_array[rd], &r_array[rs]);
+
+}
+
+void execute_slt(char **operands, Register *r_array) {
+
+    printf("Executing SLT with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rd = get_register_index(operands[0]);
+    int rs = get_register_index(operands[1]);
+    int rt = get_register_index(operands[2]);
+
+    slt(&r_array[rs], &r_array[rt], &r_array[rd]);
+
+}
+
+void execute_slti(char **operands, Register *r_array) {
+
+    printf("Executing SLTI with operands %s, %s, %s\n", operands[0], operands[1], operands[2]);
+
+    int rs = get_register_index(operands[0]);
+    int rt = get_register_index(operands[1]);
+    int imm = atoi(operands[2]);
+
+    slti(&r_array[rs], &r_array[rt], imm);
+
+}
+
+// Checks if instruction is in the table
+Instruction *find_instruction(const char *name) {
+
+    for (int i = 0; table[i].name != NULL; i++) {
+        if (strcmp(name, table[i].name) == 0) {
+            return &table[i];
+        }
+    }
+
+    return NULL;
+
+}
+
+// Creates instruction object
+Instruction *create_instruction(const char *name, char **operands, int op_count) {
+
+    Instruction *new_inst = malloc(sizeof(Instruction));
+    new_inst->name = strdup(name);
+    new_inst->operands = malloc(sizeof(char *) * op_count);
+
+    for (int i = 0; i < op_count; i++) {
+        new_inst->operands[i] = strdup(operands[i]);
+    }
+
+    return new_inst;
+
+}
+
+// Destroys instruction object
+void free_instruction(Instruction *inst) {
+
+    if (inst->name) {
+        free(inst->name);
+    }
+
+    if (inst->operands) {
+
+        for (int i = 0; i < inst->op_count; i++) {
+
+            if (inst->operands[i]) {
+                free(inst->operands[i]);
+            }
+
+        }
+
+        free(inst->operands);
+    }
+
+    free(inst);
+
+}
+
+// R-Type Instructions
+void r_add(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value + rt->value; // rd = rs + rt
+
+}
+
+void r_sub(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value - rt->value; // rd = rs - rt
+
+}
+
+void r_mult(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value * rt->value; // rd = rs * rt
+
+}
+
+void r_and(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value & rt->value; // rd = rs & rt
+
+}
+
+void r_or(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value | rt->value; // rd = rs | rt
+
+}
+
+void r_sll(Register *rt, Register *rd, int shamt) {
+
+    if (shamt < 0 || shamt > 31) {
+        printf("SHAMT must be between 0 and 31\n");
+        return;
+    }
+
+    rd->value = rt->value << shamt; // rd = rt << shamt
+
+}
+
+// I-Type Instructions
+void i_addi(Register *rs, Register *rt, int imm) {
+
+    rt->value = rs->value + imm; // rt = rs + imm
+    
+}
+
+void i_lui(Register *rt, int16_t imm) {
+
+    rt->value = (int32_t)imm << 16; // rt = imm << 16 (upper 16 bits)
+
+}
+
+void move(Register *rd, Register *rs) {
+
+    rd->value = rs->value;
+
+}
+
+void slt(Register *rs, Register *rt, Register *rd) {
+
+    rd->value = rs->value < rt->value ? 1 : 0;
+
+}
+
+void slti(Register *rs, Register *rt, int imm) {
+
+    rt->value = rs->value < imm ? 1 : 0;
 
 }
