a547a562268f2bd6fb05b6719243fad8fe2a19f2
Paludeto <gpalu54@gmail.com>
2025-02-13 18:15:12 -0300

    nomes

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+/**
+ * @file main.c
+ * @author Gabriel Paludeto, Julia Romanetto Dos Santos
+ * @brief 
+ * @version 0.1
+ * @date 2025-02-13
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
